82095df26d248f6d14bc11ec23a83c2a27723209
yzbkaka <yzbkaka@qq.com>
2020-01-08 23:28:02 +0800

    知识体系二级分页list

--- app/src/main/java/com/example/yzbkaka/kakaAndroid/ui/tree/TreeListFragment.java
+++ app/src/main/java/com/example/yzbkaka/kakaAndroid/ui/tree/TreeListFragment.java
@@ -2,10 +2,65 @@ package com.example.yzbkaka.kakaAndroid.ui.tree;
 
 import com.example.yzbkaka.kakaAndroid.bean.Article;
 import com.example.yzbkaka.kakaAndroid.inter.OnArticleListItemClickListener;
+import com.example.yzbkaka.kakaAndroid.ui.adapter.BaseListAdapter;
+import com.example.yzbkaka.kakaAndroid.ui.base.BaseAbListFragment;
+
+import java.util.List;
 
 /**
  * Created by yzbkaka on 20-1-8.
  */
 
 public class TreeListFragment extends BaseAbListFragment<TreeListPresenter, Article> implements TreeListContract.ITreeListView, OnArticleListItemClickListener {
+
+
+    @Override
+    public void onItemClick(int position, Article bean) {
+
+    }
+
+    @Override
+    public void setData(List<Article> data) {
+
+    }
+
+    @Override
+    public void onDeleteCollectClick(int position, int id, int originId) {
+
+    }
+
+    @Override
+    public void onCollectClick(int position, int id) {
+
+    }
+
+    @Override
+    public int getCid() {
+        return 0;
+    }
+
+    @Override
+    public int getArticleId() {
+        return 0;
+    }
+
+    @Override
+    public void collect(boolean isCollect, String result) {
+
+    }
+
+    @Override
+    protected TreeListPresenter createPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void loadDatas() {
+
+    }
+
+    @Override
+    protected BaseListAdapter<Article> getListAdapter() {
+        return null;
+    }
 }
--- app/src/main/java/com/example/yzbkaka/kakaAndroid/ui/tree/TreeListPresenter.java
+++ app/src/main/java/com/example/yzbkaka/kakaAndroid/ui/tree/TreeListPresenter.java
@@ -1,7 +1,11 @@
 package com.example.yzbkaka.kakaAndroid.ui.tree;
 
+import com.example.yzbkaka.kakaAndroid.bean.Article;
 import com.example.yzbkaka.kakaAndroid.core.model.impl.TreeListModel;
 import com.example.yzbkaka.kakaAndroid.core.presenter.BasePresenter;
+import com.example.yzbkaka.kakaAndroid.net.callback.RxPageListObserver;
+
+import java.util.List;
 
 /**
  * Created by yzbkaka on 20-1-8.
@@ -14,25 +18,40 @@ public class TreeListPresenter extends BasePresenter<TreeListContract.ITreeListV
     private TreeListContract.ITreeListView treeListView;
 
 
-    public TreeListPresenter(){
+    public TreeListPresenter() {
         this.treeListModel = new TreeListModel();
     }
 
 
     @Override
     public void loadTreeList() {
+        treeListView = getView();
+        RxPageListObserver<Article> mTreeListRxPageListObserver = new RxPageListObserver<Article>(this) {
 
+            @Override
+            public void onSuccess(List<Article> mData) {
+                treeListView.setData(mData);
+                if(treeListView.getData().size() == 0){
+                    treeListView.showEmpty();
+                }else {
+                    treeListView.showContent();
+                }
+            }
+
+            @Override
+            public void onFail(int errorCode, String errorMsg) {
+                treeListView.showFail(errorMsg);
+            }
+        };
+        treeListModel.getTreeList(treeListView.getPage(), treeListView.getCid(), mTreeListRxPageListObserver);
+        addDisposable(mTreeListRxPageListObserver);
     }
 
 
     @Override
-    public void collectArticle() {
-
-    }
+    public void collectArticle() {}
 
 
     @Override
-    public void unCollectArticle() {
-
-    }
+    public void unCollectArticle() {}
 }
